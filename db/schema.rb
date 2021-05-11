55a5be0e75fb91d3143f725edcf9bcfa36da17b8
jackcusick95 <jackcusick95@gmail.com>
2021-05-11 15:15:33 -0400

    migrated from sqlite3 db to postgsql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_05_11_020255) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "password_digest", null: false
