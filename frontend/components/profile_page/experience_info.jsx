c2aef3eb15433803a320e0d5fdb897727856cadd
jackcusick95 <jackcusick95@gmail.com>
2021-06-07 15:41:34 -0400

    updated componentdidmount on profile page. page now loads without having to refresh

--- frontend/components/profile_page/experience_info.jsx
+++ frontend/components/profile_page/experience_info.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 class ExperienceInfo extends React.Component {
     constructor(props) {
@@ -9,14 +8,14 @@ class ExperienceInfo extends React.Component {
 
     componentDidMount() {
         this.props.fetchAllJobs();
-        this.props.currentjob.title;
-        this.props.currentjob.company;
-        this.props.currentjob.photoUrl;
-        this.props.currentjob.start_date;
-        this.props.currentjob.end_date;
-        this.props.currentjob.location;
-        this.props.currentjob.description;
-        this.props.jobsArr;
+        // this.props.currentjob.title;
+        // this.props.currentjob.company;
+        // this.props.currentjob.photoUrl;
+        // this.props.currentjob.start_date;
+        // this.props.currentjob.end_date;
+        // this.props.currentjob.location;
+        // this.props.currentjob.description;
+        // this.props.jobsArr;
     }
 
 
@@ -26,12 +25,6 @@ class ExperienceInfo extends React.Component {
             <div className="experience-box">
                 <h1 className="about-header">Experience</h1>
                     {[...this.props.jobsArr].map((job) => {
-                        // let jobDescription;
-                        // if (job.description) {
-                        //     jobDescription = (
-                        //         <p className="job-description">{job.description}</p>
-                        //     );
-                        // }
                         const jobDescription = job.description ? <p className="job-description">{job.description}</p> : <br className="job-description-two"></br>;
                         if (job.user_id == this.props.currentuser.id) 
                         return (
@@ -46,7 +39,6 @@ class ExperienceInfo extends React.Component {
                                     <p className="job-date">{job.start_date} - {job.end_date}</p>
                                     <p className="job-location">{job.location}</p>
                                     {jobDescription}
-                                    {/* <p className="job-description">{job.description}</p> */}
                                 </div>
                             </div>
                         );
--- frontend/components/profile_page/session_profile.jsx
+++ frontend/components/profile_page/session_profile.jsx
@@ -1,7 +1,6 @@
 
 
 import React from 'react';
-import { Link } from 'react-router-dom';
 import BasicInfo from './basic_info'; 
 import ExperienceInfo from './experience_info'; 
 
